9cea2b6b96f4847ca54ed00b54057ba79369fdaa
Jhon Romero <48453401+JhonRomero26@users.noreply.github.com>
2025-05-21 12:05:43 -0500

    chore: remove comments

--- src/utils/signal.ts
+++ src/utils/signal.ts
@@ -1,41 +1,4 @@
-// // Infer T dynamic type from value in constructor
-
 type SignalSubscriber<T> = (value: T) => void;
-
-// export function signal<T>(intialValue: T) {
-//   let value: T = intialValue;
-//   const subscribers = new Set<Subscriber>();
-
-//   const notify = () => subscribers.forEach((sub) => sub());
-
-//   const signal = new Proxy(() => value, {
-//     apply(_, _this, args: T[] | undefined) {
-//       if (args && args.length > 0) {
-//         const newValue = args[0];
-//         if (value !== newValue) {
-//           value = newValue;
-//           notify();
-//         }
-//         return undefined;
-//       }
-//       return value;
-//     },
-//   }) as any;
-
-//   signal.set = (newValue: T) => {
-//     if (newValue !== value) {
-//       value = newValue;
-//       notify();
-//     }
-//   };
-
-//   signal.subscribe = (cb: Subscriber) => {
-//     subscribers.add(cb);
-//     return () => subscribers.delete(cb);
-//   };
-
-//   return signal;
-// }
 
 export function signal<T>(initialValue: T) {
   let value = initialValue;
