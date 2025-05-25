fce26a91d671917daf92a2603f750344f88be808
Jhon A. Romero Gonzaga <romerogonzaga21@gmail.com>
2025-05-24 21:26:46 -0500

    chore: sort imports

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
-import tailwindcss from "@tailwindcss/vite";
-
 import vercel from "@astrojs/vercel";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
