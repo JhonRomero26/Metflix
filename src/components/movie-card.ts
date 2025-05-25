25b5b93be4eccbccaf9cbf2fc346062bac93a941
Jhon A. Romero Gonzaga <romerogonzaga21@gmail.com>
2025-05-24 21:27:17 -0500

    chore: change ellipsis to text-ellipsis class

--- src/components/movie-card.ts
+++ src/components/movie-card.ts
@@ -43,7 +43,7 @@ if (typeof window !== "undefined" && !customElements.get("movie-card")) {
               <svg xmlns="http://www.w3.org/2000/svg" width="1em"  height="1em" viewBox="0 0 24 24" fill="none"  stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M18 7v14l-6 -4l-6 4v-14a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4z" /></svg>
             </button>
           </div>
-          <p class="carousel-item-title ellipsis">${title}</p>
+          <p class="carousel-item-title text-ellipsis line-clamp-2">${title}</p>
           <p class="carousel-item-subtitle">${year}</p>
         </div>
       </article>
